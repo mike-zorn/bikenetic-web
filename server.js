3573b88f29cc881b3c3a712eabcf7cebb7fe1b56
Mike Atkins <apeherder@gmail.com>
2013-08-19 22:59:06 -0400

    FAVICON!!!!!!!!

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
     port = process.env.PORT || 2000;
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.favicon(__dirname +"/public/img/favicon.ico"));
 
 app.listen(port);
 console.log('Up & running @', port);
