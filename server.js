d5e8d867e9b4e9f38b5d29836e9fe47bf527f3fb
Mike Atkins <apeherder@gmail.com>
2013-08-22 22:45:21 -0400

    Added nodetime

--- server.js
+++ server.js
@@ -5,5 +5,11 @@ var express = require('express'),
 app.use(express.static(__dirname + "/public"));
 app.use(express.favicon(__dirname +"/public/img/favicon.ico"));
 
+if(process.env.NODETIME_ACCOUNT_KEY) {
+  require('nodetime').profile({
+    accountKey: process.env.NODETIME_ACCOUNT_KEY,
+    appName: 'bikenetic-web'
+  });
+}
 app.listen(port);
 console.log('Up & running @', port);
